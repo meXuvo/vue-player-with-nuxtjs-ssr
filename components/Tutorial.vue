e965fd1feddf9ee5137f3b86db5c10ec8d54dfe2
mexuvo <cse.engr.shuvo@gmail.com>
2021-09-15 01:37:20 +0600

    resulation change

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -2,7 +2,11 @@
   <div>
     <div id="app">
       <div v-if="HLSCore" class="player-container">
-          <vue-core-video-player :core="HLSCore" src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"></vue-core-video-player>
+          <!-- <vue-core-video-player  :src="currentMovie.src"></vue-core-video-player> -->
+          <vue-core-video-player @loadedmetadata="loaded" :core="HLSCore" src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"></vue-core-video-player>
+      </div>
+      <div>
+        <button type="button" class="mt-5 px-5 py-2 rounded bg-red-600" @click.prevent="goPlay">play</button>
       </div>
     </div>
   </div>
@@ -10,17 +14,31 @@
 
 <script>
 let pluginLib;
+let events;
 if(process.browser){
   pluginLib = require('@core-player/playcore-hls');
+  events = require('vue-core-video-player')
 }
 export default {
   data () {
     return {
       HLSCore:null,
+      videoEvent:null,
     }
   },
   mounted(){
     this.HLSCore = pluginLib.default
+    this.videoEvent = events.EVENTS
+    
+  },
+  methods:{
+    loaded(){
+      // this.goPlay()
+    },
+    goPlay(){
+      const player = document.querySelector('video');
+      player.play()
+    }
   }
 }
 </script>
